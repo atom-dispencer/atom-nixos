4f4a7ee3ecad3bd8d490e1b41ba64dc1bdc5d74c
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-13 21:16:44 +0000

    Reverted to NixOS-WSL default configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,41 +1,27 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-{ config, pkgs, ... }:
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
 
-  # Omit previous configuration settings...
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+  
 
-  # Add user 'ryan'
-  users.users.atom = {
-    isNormalUser = true;
-    description = "atom";
-    extraGroups = [ "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = [
-        # Replace with your own public key
-        "ssh-ed25519 <some-public-key> ryan@ryan-pc"
-    ];
-    packages = with pkgs; [
-      firefox
-    #  thunderbird
-    ];
-  };
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      X11Forwarding = true;
-      PermitRootLogin = "no"; # disable root login
-      PasswordAuthentication = false; # disable password login
-    };
-    openFirewall = true;
-  };
-
-  # Omit the rest of the configuration...
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
