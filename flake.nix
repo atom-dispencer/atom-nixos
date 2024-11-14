bf8484081664e7092661c7e9789cc21df76557db
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-14 17:38:42 +0000

    Reenabled nixvim

--- flake.nix
+++ flake.nix
@@ -11,10 +11,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    #nixvim = {
-    #  url = "github:nix-community/nixvim";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #}
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
   };
 
   outputs = { self, nixpkgs, home-manager, nixos-wsl, ... }: {
