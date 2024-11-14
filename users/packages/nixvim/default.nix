0f5cca24516f495dbc3f98321a5e1e399bbe2242
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-14 22:32:06 +0000

    moving around2

---
+++ users/packages/nixvim/default.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+
+  programs.nixvim = {
+    enable = true;
+    #defaultEditor = true;
+    #viAlias = true;
+    #vimAlias = true;
+
+    colorschemes.catppuccin.enable = true;
+    plugins.lualine.enable = true;
+  };
+}
