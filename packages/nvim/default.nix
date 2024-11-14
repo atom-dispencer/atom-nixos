d4f07d4bfc8e8fae01bf76456ffe83aae8348337
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-14 17:34:52 +0000

    Removed submodules

--- packages/nvim/default.nix
+++ packages/nvim/default.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }: {
 
-  home.file."${config.xdg.configHome}/nvim" = {
-    source = ./config/nvim;
-    recursive = true;
-  };
+  #home.file."${config.xdg.configHome}/nvim" = {
+  #source = ./config/nvim;
+  #recursive = true;
+  #};
 
-  programs.neovim = {
+  programs.nixvim = {
     enable = true;
+
+    colorschemes.catppuccin.enable = true;
+    plugins.lualine.enable = true;
   };
 }
