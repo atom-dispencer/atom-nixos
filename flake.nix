359978818a0fe4bd855a1fd5374d19778295db6e
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-18 01:02:14 +0000

    SEMI... i give up

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.atom = import ./users/atom.nix
-          }
+          };
         ];
       };
     };
