5b170e5ac60ee9c592125c9e649bd555c163a297
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-24 20:58:41 +0000

    atom

--- users/atom.nix
+++ users/atom.nix
@@ -22,10 +22,14 @@
       syntaxHighlighting.enable = true;
 
       initExtra = ''
-        PROMPT="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} ) "
+        # Green/Red arrow if the last command succeeded/failed
+        PROMPT="%(?:%{$fg_bold[green]%}%1{ %} :%{$fg_bold[red]%}%1{ %} ) "
+        # Cyan current directory
         PROMPT+="%{$fg[cyan]%}%c%{$reset_color%}"
+        # Git info
         PROMPT+=' $(git_prompt_info)'
 
+        # Git prompt configuration
         ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
         ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
         ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}%1{✗%}"
