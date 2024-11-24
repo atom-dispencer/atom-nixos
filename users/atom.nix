700848edf34cdde579873005cc477211446f42fc
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-24 21:13:30 +0000

    colours

--- users/atom.nix
+++ users/atom.nix
@@ -22,21 +22,20 @@
       syntaxHighlighting.enable = true;
 
       initExtra = ''
-        ATOM_PURPLE=$(tput setaf 129)
-        # Green/Red arrow if the last command succeeded/failed
-        PROMPT="%(?:%{$fg_bold[ATOM_PURPLE]%}%1{%} :%{$fg_bold[red]%}%1{%} ) "
-        # User in white
-        PROMPT+="%{$fg[white]%}%n%{$reset_color%} "
-        # Cyan current directory
-        PROMPT+="%{$fg[cyan]%}%c%{$reset_color%} "
-        # Git info
-        PROMPT+="$(git_prompt_info)"
-
         # Git prompt configuration
         ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
         ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
         ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}%1{✗%}"
         ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+
+        # Green/Red arrow if the last command succeeded/failed
+        PROMPT="%(?:%{$fg_bold[magenta]%}%1{%} :%{$fg_bold[red]%}%1{%} ) "
+        # User in white
+        PROMPT+="%{$fg[white]%}%n%{$reset_color%} "
+        # Blue current directory
+        PROMPT+="%{$fg[blue]%}%c%{$reset_color%} "
+        # Git info
+        PROMPT+="$(git_prompt_info)"
       '';
 
       oh-my-zsh = {
