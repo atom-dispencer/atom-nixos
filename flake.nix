7b1414a6bb1f8a872c30533b24115ad65535b51c
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-25 08:53:20 +0000

    nixvim2

--- flake.nix
+++ flake.nix
@@ -38,6 +38,7 @@
         modules = [
           nixos-wsl.nixosModules.wsl
           home-manager.nixosModules.home-manager
+          nixvim.homeManagerModules.nixvim
           ./system/system.nix
 
           {
