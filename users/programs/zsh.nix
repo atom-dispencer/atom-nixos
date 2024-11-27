f82bee3272e683e55639f6836b5fb956b7459786
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-27 23:41:41 +0000

    ??????

---
+++ users/programs/zsh.nix
@@ -0,0 +1,33 @@
+{
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      initExtra = ''
+        # Git prompt configuration
+        ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+        ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+        ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}%1{💔%}"
+        ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}) %{$fg[yellow]%}%1{💜%}"
+
+        # Green/Red arrow if the last command succeeded/failed
+        PROMPT="%(?:%{$fg_bold[magenta]%}%1{%} :%{$fg_bold[red]%}%1{%} ) "
+        # User in white
+        PROMPT+="%{$fg[white]%}%n%{$reset_color%} "
+        # Blue current directory
+        PROMPT+="%{$fg[blue]%}%c%{$reset_color%} "
+        # Git info (MUST be single ' quotes to make it a STRING and NOT evaluate it now)
+        PROMPT+='$(_omz_git_prompt_info)'
+      '';
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "git-prompt"
+        ];
+      };
+    };
+}
