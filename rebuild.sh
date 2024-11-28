92f54cdbf0ae1ff16c2bd8b9df3af353e25b3a8e
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-28 00:49:35 +0000

    COOOLL

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,9 @@
+echo "  Fetching Git repository..."
 sudo git pull
+
+echo "  Updating submodules..."
 sudo git submodule update --init --recursive
 sudo git add . -A
+
+echo "  Rebuilding NixOS..."
 sudo nixos-rebuild switch --flake .\?submodules=1
