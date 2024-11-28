3bd280fc388d7af0f0f056c19d57c085b6308628
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-28 16:57:10 +0000

    bash

--- rebuild.sh
+++ rebuild.sh
@@ -1,18 +1,24 @@
 echo "  Fetching Git repository..."
 git pull
 
+
 echo "  Updating submodules..."
 git submodule update --init --recursive
 git add . -A
 
+
 echo "  Rebuilding NixOS..."
 sudo nixos-rebuild switch --impure --flake .\?submodules=1
 
+
 echo "  Deploying Neovim config..." 
 NVIM_CONFIG='~/.config/nvim'
-if [ ! -d "'$NVIM_CONFIG'/.git" ]; then
-  git clone https://github.com/atom-dispencer/atom-nvim.git "$NVIM_CONFIG"
+
+rm "$NVIM_CONFIG" -rf
+if [ ! $? -eq 0 ]; then
+  echo "Couldn't delete old Neovim configuration"
 fi
+
 cd "$NVIM_CONFIG"
 git checkout master
 git pull
