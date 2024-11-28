f84069ef9e8934d51c2facdb3da1fd8d028e360d
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-28 17:49:54 +0000

    x

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nikpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/nixos-wsl";
 
     home-manager = {
