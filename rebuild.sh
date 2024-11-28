316377b7033d5e5cfd80b36b29c7328af6109805
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-11-28 16:36:19 +0000

    bashin

--- rebuild.sh
+++ rebuild.sh
@@ -1,12 +1,18 @@
 echo "  Fetching Git repository..."
-sudo git pull
+git pull
 
 echo "  Updating submodules..."
-sudo git submodule update --init --recursive
-sudo git add . -A
-
-#echo "  Preparing for symlinking..."
-#sudo rm /home/atom/.config/nvim -r
+git submodule update --init --recursive
+git add . -A
 
 echo "  Rebuilding NixOS..."
 sudo nixos-rebuild switch --impure --flake .\?submodules=1
+
+echo "  Deploying Neovim config..." 
+$NVIM_CONFIG="~/.config/nvim"
+if [ ! -d "$NVIM_CONFIG/.git" ]; then
+  git clone https://github.com/atom-dispencer/atom-nvim.git $NVIM_CONFIG
+fi
+cd $NVIM_CONFIG
+git checkout master
+git pull
--- users/programs/nvim.nix
+++ users/programs/nvim.nix
@@ -2,20 +2,5 @@
 
   programs.neovim = {
     enable = true;
-    #defaultEditor = true;
-    #viAlias = true;
-    #vimAlias = true;
-
-    #colorschemes.catppuccin.enable = true;
-    #plugins.lualine.enable = true;
-  };
-
-  home.file.".config/nvim" = {
-    source = mkOutOfStoreSymlink {
-      builtins.fetchGit {
-        url = "https://github.com/atom-dispencer/atom-nvim.git";
-  #     rev = "1ce7253620f681a417e32a7cc8b8d3fc3be13fa9";
-      };
-    }
   };
 }
