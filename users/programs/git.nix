1c1c1d31c7e68e50f2ff0c1488e49733ba0fa9e5
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-12-08 16:08:34 +0000

    git gpg.format set?

--- users/programs/git.nix
+++ users/programs/git.nix
@@ -5,5 +5,9 @@
     userEmail = "46679475+atom-dispencer@users.noreply.github.com";
     signing.signByDefault = true;
     signing.key = "~/.ssh/github_atom-nixos.pub";
+
+    extraConfig = {
+      gpg.format = "ssh";
+    };
   };
 }
