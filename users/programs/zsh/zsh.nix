0a8607658b8eba7d359e2a6a7083c203cdc960c7
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2025-02-23 15:43:05 +0000

    Betterer enabling

--- users/programs/zsh/zsh.nix
+++ users/programs/zsh/zsh.nix
@@ -8,8 +8,6 @@
       completionInit = ''
         # Use tab to complete commands
         bindkey '	' autosuggest-accept
-        # Enable git completions (and others)
-        autoload -U compinit && compinit -i
       '';
 
       initExtra = builtins.readFile ./initExtra.zsh;
