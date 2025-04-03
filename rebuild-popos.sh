a625f8c597493f2395a1dd94fc6f28d702213cd1
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2025-04-03 10:41:54 +0000

    command

---
+++ rebuild-popos.sh
@@ -0,0 +1,22 @@
+echo "  Fetching Git repository..."
+git reset --hard HEAD
+git pull
+
+
+echo "  Updating submodules..."
+git submodule update --init --recursive
+git add . -A
+
+
+echo "  Rebuilding NixOS..."
+sudo nix run home-manager -- switch --flake .#popos\?submodules=1
+if [ ! $? -eq 0 ]; then
+echo "  Failed to rebuild Nix Home Manager!"
+  exit 1
+fi
+
+
+echo "  Deploying Neovim config..." 
+NVIM_CONFIG=~/.config/nvim
+rm "$NVIM_CONFIG" -rf
+git clone https://github.com/atom-dispencer/atom-nvim.git "$NVIM_CONFIG"
